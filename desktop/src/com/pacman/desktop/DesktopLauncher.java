b96cc6b58f2e9ef70a55f78c21ad21453cdbf503
Agustin-Vidal <agustinnicolasvidal98@gmail.com>
2024-10-31 20:00:30 -0300

    Add files via upload

---
+++ desktop/src/com/pacman/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.pacman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pacman.JuegoPrincipal;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        System.setProperty("user.name", "gasty");
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new JuegoPrincipal(new BaseDeDatosDesktop()), config);
+    }
+}
